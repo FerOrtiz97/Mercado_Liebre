164e49aa8c688e2685f618097ba6f413d6048b40
FerOrtiz97 <tino.pipe97@gmail.com>
2023-11-15 00:43:33 -0300

    agregue las rutas login y registre

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 8000;
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 app.listen(port, () => {
@@ -11,5 +14,22 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
+
+app.get ("/login", function(req,res){
+    res.sendFile(path.join (__dirname, "/views/login.html"))
+});
+app.post("/login", (req,res) => {
+    console.log(req.body);
+res.redirect("/");
+})
+
+app.get ("/register", function(req,res){
+    res.sendFile(path.join (__dirname, "/views/register.html"))
+});
+app.post("/register", (req,res) => {
+    console.log(req.body);
+res.redirect("/");
+})
+
 
 app.use(express.static(__dirname + '/public'));
